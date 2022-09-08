0f2250cf7fb30a728868997950da629075a1e343
Kundan <kundan kamble>
2022-09-08 11:19:05 -0800

    Connect to database

---
+++ AddressBook_ADO.NET/AddressBookRepo.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook_ADO.NET
+{
+    class AddressBookRepo
+    {
+        public static string connectionString = @"Data Source=(localdb)\MSSQLLOCALDB;Initial Catalog=AddressBook;Integrated Security=True";
+        SqlConnection connection = new SqlConnection(connectionString);
+        public void checkConnection()
+        {
+            try
+            {
+                this.connection.Open();
+                Console.WriteLine("connection established");
+                this.connection.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+    }
+}
---
+++ AddressBook_ADO.NET/Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace AddressBook_ADO.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to AddressBookSystem using Ado.net");
+            AddressBookRepo addressBookRepo = new AddressBookRepo();
+            addressBookRepo.checkConnection();
+        }
+    }
+}
